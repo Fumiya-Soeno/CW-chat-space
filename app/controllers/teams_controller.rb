45498b6e0940034c028a398ffae1d4e63cb11dc1
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-03-11 20:22:42 +0900

    edit-teams-controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,34 @@
 class TeamsController < ApplicationController
+  before_action :set_user
+  before_action :set_teams_params
 
   def index
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to user_teams_path(@user), notice: 'チームを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def set_user
+    @user = User.find(current_user)
+  end
+
+  def set_teams_params
+    teams = Team.select(:name,:char)
+  end
+
+  private
+  def team_params
+    params.require(:team).permit(:name,:char).merge(user_id: current_user.id)
   end
 
 end
